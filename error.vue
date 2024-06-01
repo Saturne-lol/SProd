bb95554534d779df0124b5b55c5f3d1e21bbd526
Cleboost <clement.balarot@gmail.com>
2024-06-01 21:24:41 +0200

    Update error.vue

--- error.vue
+++ error.vue
@@ -4,24 +4,25 @@ import type {NuxtError} from '#app'
 const props = defineProps({
   error: Object as () => NuxtError
 })
-
-if (process.client) {
-  switch (props?.error?.statusCode) {
-    case 500:
-      window.location.href = '/500?error=500'
-      break
-    case 404:
-      window.location.href = '/404?type=404'
-      break
-    default:
-      window.location.href = `/${props?.error?.statusCode}`
-      break
-  }
-}
+//
+// if (process.client) {
+//   switch (props?.error?.statusCode) {
+//     case 500:
+//       window.location.href = '/500?error=500'
+//       break
+//     case 404:
+//       window.location.href = '/404?type=404'
+//       break
+//     default:
+//       window.location.href = `/${props?.error?.statusCode}`
+//       break
+//   }
+// }
 </script>
 
 <template>
   <div>
     <h1>Error</h1>
+    <h3>{{error}}</h3>
   </div>
 </template>
