aff5bb5cfd62116ec72b9798b34f5eb62a233439
Cleboost <clement.balarot@gmail.com>
2024-06-01 21:01:31 +0200

    blacklist

--- server/api/profile/is-exist.ts
+++ server/api/profile/is-exist.ts
@@ -3,8 +3,13 @@ import {PrismaClient} from "@prisma/client";
 const prisma = new PrismaClient()
 
 export default defineEventHandler(async (event) => {
-    const url = new URLSearchParams(event.req.url)
     if (!getQuery(event)?.username) return {code: false}
-    if (!await prisma.setting.findFirst({where: {url: getQuery(event)?.username as string}})) return {code: false}
+    const user = await prisma.setting.findFirst({
+        where: {url: getQuery(event)?.username as string},
+        select: {account: {select: {blacklisted: true}}}
+    })
+    
+    if (!user) return {code: "ne"}
+    if (user.account.blacklisted) return {code: "bl"}
     return {code: true}
 })
