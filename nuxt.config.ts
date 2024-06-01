21723785daee2abeb8d5a86ff8b35803e77c16e5
Cleboost <clement.balarot@gmail.com>
2024-06-01 21:01:52 +0200

    module update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: {enabled: true},
-    modules: ['nuxt-icon', 'nuxt-obfuscator', "nuxt-gtag"],
+    modules: ['nuxt-icon', 'nuxt-obfuscator', "nuxt-gtag", "@nuxtjs/sitemap"],
     obfuscator: {
         // default config:
         mapFile: 'obfuscation.map.json',
