5a9f0445566d25231f8c65c08381ca75594c8976
Cleboost <clement.balarot@gmail.com>
2024-06-01 16:56:00 +0200

    fast discord api

--- api/bot.ts
+++ api/bot.ts
@@ -1,33 +1,36 @@
 import {Client, GatewayIntentBits, Partials} from "discord.js";
 
-export default async function createBot(): Promise<Client> {
-    return await new Promise((resolve, reject) => {
-        const client = new Client({
-            intents: [
-                GatewayIntentBits.DirectMessageReactions,
-                GatewayIntentBits.DirectMessageTyping,
-                GatewayIntentBits.DirectMessages,
-                GatewayIntentBits.GuildBans,
-                GatewayIntentBits.GuildEmojisAndStickers,
-                GatewayIntentBits.GuildIntegrations,
-                GatewayIntentBits.GuildIntegrations,
-                GatewayIntentBits.GuildInvites,
-                GatewayIntentBits.GuildMembers,
-                GatewayIntentBits.GuildMessageReactions,
-                GatewayIntentBits.GuildMessageTyping,
-                GatewayIntentBits.GuildMessages,
-                GatewayIntentBits.GuildPresences,
-                GatewayIntentBits.GuildScheduledEvents,
-                GatewayIntentBits.GuildScheduledEvents,
-                GatewayIntentBits.GuildVoiceStates,
-                GatewayIntentBits.GuildWebhooks,
-                GatewayIntentBits.Guilds,
-                GatewayIntentBits.MessageContent,
-            ],
-            partials: [Partials.Channel, Partials.User, Partials.Reaction, Partials.Message, Partials.GuildMember, Partials.GuildScheduledEvent, Partials.ThreadMember],
-        });
-        client.login("MTE5OTM4NTMyODc4NDUwNjkzMQ.Gero_4.ULTCyXXAUKnYGgwn13bOc695RqFNo_0vzQGJDk").then(() => {
-            resolve(client)
-        }).catch(reject)
-    })
+let client: Client | undefined;
+
+export function createBot() {
+    client = new Client({
+        intents: [
+            GatewayIntentBits.DirectMessageReactions,
+            GatewayIntentBits.DirectMessageTyping,
+            GatewayIntentBits.DirectMessages,
+            GatewayIntentBits.GuildBans,
+            GatewayIntentBits.GuildEmojisAndStickers,
+            GatewayIntentBits.GuildIntegrations,
+            GatewayIntentBits.GuildIntegrations,
+            GatewayIntentBits.GuildInvites,
+            GatewayIntentBits.GuildMembers,
+            GatewayIntentBits.GuildMessageReactions,
+            GatewayIntentBits.GuildMessageTyping,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.GuildPresences,
+            GatewayIntentBits.GuildScheduledEvents,
+            GatewayIntentBits.GuildScheduledEvents,
+            GatewayIntentBits.GuildVoiceStates,
+            GatewayIntentBits.GuildWebhooks,
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.MessageContent,
+        ],
+        partials: [Partials.Channel, Partials.User, Partials.Reaction, Partials.Message, Partials.GuildMember, Partials.GuildScheduledEvent, Partials.ThreadMember],
+    });
+    client.login("MTE5OTM4NTMyODc4NDUwNjkzMQ.Gero_4.ULTCyXXAUKnYGgwn13bOc695RqFNo_0vzQGJDk");
+    client.once("ready", () => {console.log("Bot is ready")})
+}
+
+export function getBot() {
+    return client;
 }
--- api/discord.ts
+++ api/discord.ts
@@ -1,5 +1,8 @@
 import type {H3Event} from "h3";
 import axios from "axios";
+import {PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient()
 
 export async function checkToken(event: H3Event): Promise<{
     id: string,
@@ -19,6 +22,21 @@ export async function checkToken(event: H3Event): Promise<{
 
     if (typeof resDsc !== "boolean" && resDsc?.status !== 200) return false
     if (!resDsc) return false
+
+    if (typeof resDsc !== "boolean" && resDsc?.data?.id) {
+        if (!await prisma.account.findFirst({where: {id: resDsc.data.id}})) {
+            await prisma.account.create({
+                data: {
+                    id: resDsc.data.id,
+                }
+            })
+            await prisma.setting.create({
+                data: {
+                    account_id: resDsc.data.id
+                }
+            })
+        }
+    }
 
     return typeof resDsc !== "boolean" ? resDsc?.data : false
 }
