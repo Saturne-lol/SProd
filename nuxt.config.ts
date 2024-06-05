8f63bc8aa158cabce7dbb30bb9d75bfc5aad5f28
Cleboost <clement.balarot@gmail.com>
2024-06-05 22:54:25 +0200

    add nuxt typed router

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: {enabled: true},
-    modules: ['nuxt-icon', "nuxt-gtag", "@nuxtjs/sitemap", "@nuxt/ui"],
+    modules: [
+        'nuxt-icon',
+        "nuxt-gtag",
+        "@nuxtjs/sitemap",
+        "@nuxt/ui",
+        "nuxt-typed-router"
+    ],
     components: true,
     gtag: {
         id: 'G-YKC5TQ8C98'
