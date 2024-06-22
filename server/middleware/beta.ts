2287136d880ddd01f94b898646eb03c9459ed322
Cleboost <clement.balarot@gmail.com>
2024-06-22 16:41:56 +0200

    fix redirect login url

--- server/middleware/beta.ts
+++ server/middleware/beta.ts
@@ -5,7 +5,7 @@ const prisma = new PrismaClient()
 export default defineEventHandler(async (event) => {
     if (getRequestURL(event).toString().includes('update-')) {
         const user = await checkToken(event)
-        if (!user) return sendRedirect(event, "/auth/login")
+        if (!user) return sendRedirect(event, "/login")
         const beta = await prisma.account.findFirst({where: {id: user?.id}, select: {beta: true}})
         if (!beta?.beta) return new Response("You are not a beta tester", {status: 403})
     }
