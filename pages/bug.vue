26cba49b4489dd086bc0512ff5ee8e8c9d09ded0
Cleboost <clement.balarot@gmail.com>
2024-06-22 16:43:48 +0200

    start bug report page

---
+++ pages/bug.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+import { ref } from 'vue'
+
+const title = ref('')
+const description = ref('')
+
+function submit() {
+  console.log(title.value, description.value)
+}
+</script>
+
+<template>
+  <label>Title</label>
+  <br/>
+  <input type="text" v-model="title"/>
+  <br/><br/>
+  <label>Description</label>
+  <br/>
+  <textarea v-model="description"></textarea>
+</template>
+
+<style scoped>
+  input {
+    box-sizing: border-box;
+    border: 2px solid #ccc;
+  }
+
+  textarea {
+    box-sizing: border-box;
+    border: 2px solid #ccc;
+  }
+</style>
