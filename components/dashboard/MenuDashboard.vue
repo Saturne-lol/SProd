96cccbbedfed580cb5d8c49c42bec83ad96c3f85
ADsglt77 <149898260+ADsglt77@users.noreply.github.com>
2024-06-23 23:29:36 +0200

    Nav Bar Dash (add soon)

--- components/dashboard/MenuDashboard.vue
+++ components/dashboard/MenuDashboard.vue
@@ -27,18 +27,30 @@ const { data } = await useFetch("/api/account/get-nav") as {
             </a>
           </li>
           <li>
+            <div class="soon">
+              <img src="/public/img/loading.gif" alt="">
+              <h1>SOON . . .</h1>
+            </div>
             <a href="/dashboard/links">
               <Icon name="ph:link-simple-bold" class="Icon" />
               YOUR LINKS
             </a>
           </li>
           <li>
+            <div class="soon">
+              <img src="/public/img/loading.gif" alt="">
+              <h1>SOON . . .</h1>
+            </div>
             <a href="/dashboard/stats">
               <Icon name="ion:stats-chart" class="Icon" />
               YOUR STATS
             </a>
           </li>
           <li>
+            <div class="soon">
+              <img src="/public/img/loading.gif" alt="">
+              <h1>SOON . . .</h1>
+            </div>
             <a href="/dashboard/contact">
               <Icon name="material-symbols:alternate-email-rounded" class="Icon" />
               CONTACT US
@@ -108,6 +120,7 @@ const { data } = await useFetch("/api/account/get-nav") as {
 }
 
 .nav .background .links li {
+  position: relative;
   background-color: rgba(255, 255, 255, 0.1);
   border: 1px solid rgba(255, 255, 255, 0.4);
   padding: 10px 45px;
@@ -121,6 +134,40 @@ const { data } = await useFetch("/api/account/get-nav") as {
 .nav .background .links .Icon {
   margin-right: 20px;
 }
+
+
+
+
+
+
+
+
+.nav .background .links .soon {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 10;
+  backdrop-filter: blur(3px);
+  border-radius: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center
+}
+
+.nav .background .links .soon h1 {
+  font-size: 120%;
+  font-weight: 700;
+  text-shadow: 0 0 25px #000000;
+}
+
+.nav .background .links .soon img {
+  margin-right: 10px;
+  width: 20px;
+  height: 20px;
+}
+
 
 .nav .background .profil {
   width: 100%;
