1e3ccc02601e85fac8abe7078ee5b41b94aab14a
Cleboost <clement.balarot@gmail.com>
2024-06-24 21:52:42 +0200

    new bot api

--- server/api/profile/get-box-user.ts
+++ server/api/profile/get-box-user.ts
@@ -1,14 +1,12 @@
-import {getBot} from "~/api/bot";
 import {PrismaClient} from "@prisma/client";
-import {Client} from "discord.js";
+import axios from "axios";
 
 const prisma = new PrismaClient()
 
+// noinspection JSUnusedGlobalSymbols
 export default defineEventHandler(async (event) => {
     if (!getQuery(event).username) return {error: "No username provided"}
-    const discordClient = getBot() as Client
 
-    // const settings = (await bdd`SELECT username, description, account FROM settings WHERE url = ${url}`)[0]
     const user = await prisma.account.findFirst({
         where: {
             setting: {
@@ -19,13 +17,16 @@ export default defineEventHandler(async (event) => {
             id: true
         }
     });
+
     if (!user) return {error: "Invalid username"}
-    const member = (await (await discordClient.guilds.fetch("1129015826410901556")).members.fetch(user?.id as string))
-    discordClient.destroy()
+    const member = await axios.get("https://bot.saturne.lol/member/" + user.id).then((res) => {
+        return res.data
+    }).catch(() => false)
 
     return {
-        avatar: member.user.displayAvatarURL({extension: "png"}),
-        username: member.user.displayName || "Invalid username",
-        status: member.presence?.activities[0]?.state || null
+        avatar: member.avatar || "https://cdn.saturne.lol/file/profile/nooo.png",
+        username: member.displayName || "Invalid username",
+        status: null,
+        presence: member.presence
     }
 })
