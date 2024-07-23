8efd53dfe6509048f2e2345d3ff4b3a93efcad9d
Cleboost <clement.balarot@gmail.com>
2024-07-23 19:37:26 +0200

    feat: global dashboard function

--- composables/dashboard.ts
+++ composables/dashboard.ts
@@ -1,5 +1,7 @@
 import customize from "~/composables/dashboard/customize";
+import global from "~/composables/dashboard/global";
 
 export default {
+    global: global,
     customize: customize
 }
