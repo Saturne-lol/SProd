e086ca7c4389e03f6e0687ad52ffe660eacdb938
Cleboost <clement.balarot@gmail.com>
2024-07-24 13:02:45 +0200

    feat: add chart (data not set)

--- pages/dashboard/stats.vue
+++ pages/dashboard/stats.vue
@@ -1,13 +1,31 @@
 <script setup lang="ts">
+import { Line } from 'vue-chartjs'
+const chartData = ref({
+  labels: ['January', 'February', 'March', 'April', 'May'],
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56],
+    fill: false,
+    borderColor: 'rgb(75, 192, 192)',
+    tension: 0.4
+  }]
+})
 
-import FooterDashboard from "~/components/dashboard/FooterDashboard.vue";
-import MenuDashboard from "~/components/dashboard/MenuDashboard.vue";
-import MenuPhoneDashboard from "~/components/dashboard/MenuPhoneDashboard.vue";
+const chartOptions = ref({
+  scales: {
+    y: {
+      beginAtZero: true
+    }
+  }
+})
 </script>
 
 <template>
-  <div class="content">
-    STATS
+  <div>
+    <Line
+        :data="chartData"
+        :options="chartOptions"
+    />
   </div>
 </template>
 
