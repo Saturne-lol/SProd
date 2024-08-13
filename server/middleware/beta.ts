153557fd40ce3a2df6c8e51f699c54a99a5f38b5
Cleboost <clement.balarot@gmail.com>
2024-08-13 18:52:01 +0200

    feat: beta need

--- server/middleware/beta.ts
+++ server/middleware/beta.ts
@@ -3,7 +3,9 @@ import {PrismaClient} from "@prisma/client";
 
 const prisma = new PrismaClient()
 export default defineEventHandler(async (event) => {
-    if (getRequestURL(event).toString().includes('update-')) {
+    if (getRequestURL(event).toString().includes('update-') ||
+        getRequestURL(event).toString().includes('dashboard/customize/set-')
+    ) {
         const user = await checkToken(event)
         if (!user) return sendRedirect(event, "/login")
         const beta = await prisma.account.findFirst({where: {id: user?.id}, select: {beta: true}})
