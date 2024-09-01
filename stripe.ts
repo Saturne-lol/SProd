fcf53b319ea28e273fc7e07759f089ff4adccf33
Cleboost <clement.balarot@gmail.com>
2024-09-01 16:03:02 +0200

    fix: stripe key

--- stripe.ts
+++ stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe';
 
-const key = process.env.NODE_ENV === 'development' ? process.env.DEV_STRIPE_SECRET_KEY : process.env.STRIPE_SECRET_KEY;
+const key = process.env.NODE_ENV === 'development' ? 'sk_test_51PLVZS05yQwZajCdqYZcMZL1UmJxdmy3lEHckp7R6fPKkmeL8xipsrj4PN2a5zQNMfSwPrDoqfhizI4agzl2RspV00GTvYZiD7' : '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';
 const stripe = new Stripe(key as string);
 // noinspection JSUnusedGlobalSymbols
 export default stripe;
